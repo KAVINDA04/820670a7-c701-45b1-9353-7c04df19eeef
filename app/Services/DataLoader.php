01d162addca914dd72b8a3a84c0b3d02b4a6ab67
KAVINDA04 <akilakavinda04@gmail.com>
2025-08-21 09:08:50 +0530

    update-data-loader

--- app/Services/DataLoader.php
+++ app/Services/DataLoader.php
@@ -7,6 +7,7 @@ class DataLoader
     public static function load(string $filename): array
     {
         $path = storage_path("app/data/{$filename}");
+
         if (!file_exists($path)) {
             throw new \Exception("Data file not found: {$filename}");
         }
