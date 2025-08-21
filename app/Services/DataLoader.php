6a88cf886284b64bdc7d90e40cb52e1a0b993913
KAVINDA04 <akilakavinda04@gmail.com>
2025-08-21 09:25:06 +0530

    change-resource-path

--- app/Services/DataLoader.php
+++ app/Services/DataLoader.php
@@ -6,7 +6,7 @@ class DataLoader
 {
     public static function load(string $filename): array
     {
-        $path = storage_path("app/data/{$filename}");
+        $path = storage_path("app/public/data/{$filename}");
 
         if (!file_exists($path)) {
             throw new \Exception("Data file not found: {$filename}");
